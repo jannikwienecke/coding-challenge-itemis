a40dde4f35ecc5474101d96acbc8936943e3b373
Jannik Wienecke <wienecke.jannik@gmail.com>
2021-02-01 23:51:30 +0000

    fix

--- src/components/layout/SideContent.tsx
+++ src/components/layout/SideContent.tsx
@@ -4,13 +4,27 @@ import tw from "twin.macro"
 import "./SideContent.css"
 
 export const SideContent: React.FC = ({ children }) => {
+  const [show, setShow] = React.useState<boolean | null>(null)
   const { shoppingList } = useGlobalStore()
 
   const Wrapper = tw.div` border-l-2 flex flex-col h-full`
   const showSideContent = shoppingList.shoppingItems.length > 0
 
+  // if there is no delay - tab content will not update on the first click
+  // when the basket is empty
+  React.useEffect(() => {
+    if (shoppingList.shoppingItems.length === 0) {
+      if (show) setShow(false)
+    } else if (!show) {
+      setTimeout(() => {
+        setShow(true)
+      }, 10)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [shoppingList.shoppingItems])
+
   return (
-    <Wrapper id="side-content" className={showSideContent ? "show" : "notShow"}>
+    <Wrapper id="side-content" className={show ? "show" : "notShow"}>
       {children}
     </Wrapper>
   )
